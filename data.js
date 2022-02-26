14fb22e2c06f24f6604d38810da1b859dbb4ebad
Amrit Atwal <amritatwal04@gmail.com>
2022-02-26 21:51:12 +0000

    Changed wording in main, created functionality to update the colour of the box shadow and colour of text randomly

--- data.js
+++ data.js
@@ -1,10 +1,25 @@
 export const suggestions = [
     {
         "Title": "Go for a walk",
-        "Info": "If it's nice outside, why not go for a walk instead? You'll feel much better after!"
+        "Info": "If it's nice outside where you are, why not go for a walk instead?"
     },
     {
         "Title": "Get a cup of tea",
         "Info": "Get yourself a nice cup of tea and drink it mindfully!"
+    },
+    {
+        "Title": "Read a book",
+        "Info": "If you have a book nearby, take some time to read a chapter or two!"
     }
+]
+
+export const colours = [
+    "rgb(75, 94, 201)",
+    "orange",
+    "pink",
+    "blue",
+    "green",
+    "red",
+    "purple",
+    "yellows"
 ]
--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-import { suggestions } from "./data.js"
+import { suggestions, colours } from "./data.js"
 
 let title = document.querySelector(".title");
 let info = document.querySelector(".info");
+let activity = document.querySelector(".activity");
+let boxShadow = document.querySelector(".box")
 
+function getRandomInteger(max) {
+    const integer = Math.floor(Math.random() * (max));
+    return integer;
+}
 
-function getRandomSuggeston() {
-    let max = suggestions.length;
-    console.log(max);
-    let result = Math.floor(Math.random() * (max));
-    console.log(result)
+function getSuggeston() {
+    const max = suggestions.length;
+    const result = getRandomInteger(max);
     title.innerHTML = suggestions[result]["Title"];
     info.innerHTML = suggestions[result]["Info"];
 }
 
-getRandomSuggeston();
+function setColour() {
+    const randomInteger = getRandomInteger(colours.length);
+    const colour = colours[randomInteger];
+    activity.style.color = colour;
+    boxShadow.style.boxShadow = "0px 0px 25px 0px " + colour;
+    boxShadow.style.webkitBoxShadow = "0px 0px 25px 0px " + colour;
+    boxShadow.style.mozBoxShadow = "0px 0px 25px 0px " + colour;
+}
+
+getSuggeston();
+setColour();
