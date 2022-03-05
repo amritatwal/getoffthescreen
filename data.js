fffbb1c5842ddc2347939ea47321a8362a9e3963
Amrit Atwal <amritatwal04@gmail.com>
2022-03-05 18:15:25 +0000

    Added licence.md

--- data.js
+++ data.js
@@ -1,25 +1,42 @@
+// An array of objects that contain a suggestion (title), along with some additional information
+
 export const suggestions = [
     {
-        "Title": "Go for a walk",
-        "Info": "If it's nice outside where you are, why not go for a walk instead?"
+        "title": "Go for a walk",
+        "info": "If it is nice outside where you are, why not go for a walk instead?"
     },
     {
-        "Title": "Get a cup of tea",
-        "Info": "Get yourself a nice cup of tea and drink it mindfully!"
+        "title": "Get a drink",
+        "info": "Get yourself a drink or a nice cup of tea and drink it mindfully!"
     },
     {
-        "Title": "Read a book",
-        "Info": "If you have a book nearby, take some time to read a chapter or two!"
+        "title": "Do nothing",
+        "info": "Take 5-10 minutes to just simply do nothing and let yourself rest by taking a break"
+    },
+    {
+        "title": "Read a book",
+        "info": "If you have a book nearby, take some time to read a chapter or two!"
+    },
+    {
+        "title": "Exercise",
+        "info": "Get your blood pumping by doing a workout! How about going for a run or a quick Yoga break?"
+    },
+    {
+        "title": "Meditate",
+        "info": "Take time to anchor yourself and be present in the now"
     }
 ]
 
+// An array of colours that will change upon a new tab or refresh
+
 export const colours = [
-    "rgb(75, 94, 201)",
+    "blue",
     "orange",
     "pink",
-    "blue",
-    "green",
+    "darkgrey",
+    "aqua",
+    "seagreen",
     "red",
     "purple",
-    "yellows"
-]
+    "mustard",
+];
--- main.js
+++ main.js
@@ -1,23 +1,28 @@
+// // Import the suggestions and colours 
 import { suggestions, colours } from "./data.js"
 
+// // Declare variables used for querying the DOM
 let title = document.querySelector(".title");
 let info = document.querySelector(".info");
-let activity = document.querySelector(".activity");
+let activity = document.querySelector(".active");
 let boxShadow = document.querySelector(".box")
 
-function getRandomInteger(max) {
+// // Retrieves a random integer between 0 and an integer given as an argument 
+export function getRandomInteger(max) {
     const integer = Math.floor(Math.random() * (max));
     return integer;
 }
 
-function getSuggeston() {
+// // Retrieves a random suggestion from our array using getRandomInteger, then renders the data to display in the appropriate tags  
+export function getSuggestion() {
     const max = suggestions.length;
     const result = getRandomInteger(max);
-    title.innerHTML = suggestions[result]["Title"];
-    info.innerHTML = suggestions[result]["Info"];
+    title.innerText = suggestions[result]["title"];
+    info.innerText = suggestions[result]["info"];
 }
 
-function setColour() {
+// // Retrieves a random colour from our array using getRandomInteger, then assigns said colour to the activity text and box shadow   
+export function setColour() {
     const randomInteger = getRandomInteger(colours.length);
     const colour = colours[randomInteger];
     activity.style.color = colour;
@@ -26,5 +31,6 @@ function setColour() {
     boxShadow.style.mozBoxShadow = "0px 0px 25px 0px " + colour;
 }
 
-getSuggeston();
+// // Invoke functions 
+getSuggestion();
 setColour();
